3d555002121b1a3a5968e9d21b4ce61630dd2a3f
Shsin9797 <aaa8141@naver.com>
2023-02-20 19:44:26 +0900

    Repository & Controller & Service & View구현: Template

---
+++ src/main/java/com/kns/tenquest/controller/TemplateController.java
@@ -0,0 +1,39 @@
+package com.kns.tenquest.controller;
+
+import com.kns.tenquest.entity.Category;
+import com.kns.tenquest.entity.Template;
+import com.kns.tenquest.service.CategoryService;
+import com.kns.tenquest.service.TemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class TemplateController {
+
+    @Autowired
+    TemplateService templateService;
+
+
+    //템플릿 확인하기(전체) : GET  : 데이터 가져와서 뿌려주기
+    @RequestMapping(value="/view/template",method= RequestMethod.GET)
+    public String templateView(Model model){
+        // Temporarily implemented. Just for test.
+        List<Template> templateList = templateService.getAllTemplates();
+        model.addAttribute("templateList", templateList);
+        return "template_view";
+    }
+
+    //템플릿 추가하기 : POST  : 데이터 추가 : 개발자만 가능
+
+    //템플릿 수정하기 : UPDATE : 데이터 수정: 개발자만 가능
+
+    //템플릿 삭제하기 : DELETE : 데이터 삭제: 개발자만 가능
+
+}
+
+
